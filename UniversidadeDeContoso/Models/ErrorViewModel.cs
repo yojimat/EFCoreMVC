bde352ebabe9632e0d24651c4e1bc46a8fb3c240
ViniciusNeves <vinicius.costa@ssp.df.gov.br>
2019-09-24 22:35:43 -0300

    Adicionar arquivos de projeto.

---
+++ UniversidadeDeContoso/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UniversidadeDeContoso.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
