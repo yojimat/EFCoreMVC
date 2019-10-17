e8b57867da6597157b3f4cd38981eaa40c0c0b0c
ViniciusNeves <vinicius.costa@ssp.df.gov.br>
2019-10-17 17:54:48 -0300

    Adicioando arquivo novo

---
+++ UniversidadeDeContoso/Models/UniversidadeViewModels/DadosAtribuidosCurso.cs
@@ -0,0 +1,9 @@
+﻿namespace UniversidadeDeContoso.Models.UniversidadeViewModels
+{
+    public class DadosAtribuidosCurso
+    {
+        public int CursoId { get; set; }
+        public string Nome { get; set; }
+        public bool Atribuido { get; set; }
+    }
+}
